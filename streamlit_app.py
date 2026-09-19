ffa3227ae64a07886e8ad17f1e559ee4cadee3b9
Purav Desai <puravdesai.it23d2@scet.ac.in>
2026-09-19 17:58:28 +0530

    Revise comments and fix formatting in streamlit_app.py
    Updated comments to reflect changes in functionality and fixed a formatting issue.

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,15 +53,18 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-# Convert list to string and prepare SQL
+# Convert list to string and get nutrition information
 if ingredients_list:
+
     ingredients_string = ""
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
 
+        st.subheader(fruit_chosen + " Nutrition Information")
+
         smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/watermelon"
+            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
         )
 
         sf_df = st.dataframe(
